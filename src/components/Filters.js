d7aca428f198c081c5b6b20f59acf5cca7961964
J. Ackerman <github email address>
2017-07-21 18:07:40 -0700

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-class Filters extends React.Component {
+export default class Filters extends React.Component {
   constructor() {
     super();
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.props.onChangeType(event.target.value);
   }
 
   render() {
@@ -10,7 +16,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,11 +25,9 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
   }
 }
-
-export default Filters;
