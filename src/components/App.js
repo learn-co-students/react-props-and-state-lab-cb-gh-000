8927671b67ea32ad56be709b63e00b9d5f4f2b4b
edwin rivera <waehake@gmail.com>
2018-01-31 14:55:28 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,34 @@ class App extends React.Component {
     };
   }
 
+  onChangeType = (type) => {
+    this.setState({
+      filters: Object.assign({}, this.state.filters, {
+        type: type,
+      })
+    });
+  }
+
+  //add the ID of the adobpted pet to state
+  onAdoptPet = (id) => {
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, id],
+    });
+  };
+
+
+  onFindPetsClick = (type) => {
+    var url = '/api/pets';
+    //create alternate URLS if the type isnt all
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`;
+    }
+    //use fetch to add the data to the pets state
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({pets}));
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -25,10 +53,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                filters={this.state.filters}
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                pets={this.state.pets}
+                adoptedPets={this.state.adoptedPets}
+                onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
