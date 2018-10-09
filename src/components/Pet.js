4b135d9cbfebe75e976e4481d34b5f2acd818ca9
Berry <bsemexan@gmail.com>
2018-10-09 10:52:10 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,35 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor() {
+    super();
+    this.handleAdopt = this.handleAdopt.bind(this);
+  }
+
+  handleAdopt(){
+    this.props.onAdoptPet(this.props.pet.id)
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.name}
+            {this.props.pet.gender==="male"?"♂":"♀"} 
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+        {this.props.pet.isAdopted && <button className="ui disabled button">Already adopted</button>}
+        {!this.props.pet.isAdopted && <button className="ui primary button"onClick={this.handleAdopt}>Adopt pet</button>}
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,14 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    const pets = this.props.pets.map((pet, index) => (
+      <Pet 
+        pet={pet} 
+        key={index} 
+        onAdoptPet={this.props.onAdoptPet} 
+        isAdopted={this.props.pets.includes(pet.id)} />
+    ));
+    return <div className="ui cards">{pets}</div>
   }
 }
 
